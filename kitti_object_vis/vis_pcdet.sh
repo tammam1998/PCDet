76260e97bc7f9464a2140b9c5a8cf1b4ad7e3b1e
Tammam Mustafa <tammam@pop-os.localdomain>
2020-06-05 10:57:56 -0400

    fixed directories

--- kitti_object_vis/vis_pcdet.sh
+++ kitti_object_vis/vis_pcdet.sh
@@ -1,7 +1,7 @@
-cp -r ~/ml_urop/PCDet/data/kitti/testing/velodyne/* data/object/training/velodyne/
+cp -r ../data/kitti/testing/velodyne/* data/object/training/velodyne/
 
-cp -r ~/ml_urop/PCDet/output/second/default/eval/epoch_no_number/test/default/final_result/data/* data/object/training/label_2/
+cp -r ../output/second/default/eval/epoch_no_number/test/default/final_result/data/* data/object/training/label_2/
 
-cp -r ~/ml_urop/PCDet/output/PartA2_fc/default/eval/epoch_2/test/default/final_result/data/* data/object/training/label_2/
+# cp -r ../output/PartA2_fc/default/eval/epoch_2/test/default/final_result/data/* data/object/training/label_2/
 
 python kitti_object.py --show_lidar_with_depth --img_fov --const_box --vis --ind 10
